549a105d928bc9717e01888effbe31a3ccf55c11
Jizin <ji.dozin@gmail.com>
2021-07-19 22:09:37 +0900

    .

---
+++ php_variable.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>String exam</h1>
+        <?php
+            $old="21";
+            echo "Hi! I am ".$old."years old, <br>Is there any ".$old." age friend here?"
+        ?>
+    </body>
+</html>
---
+++ string_ex.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>String exam</h1>
+        <?php
+            echo "Hello world<br>";
+            echo 'Hello world<br>';
+            echo "Hello \"world\"<br>";
+            echo "Hello "."World<br>";
+            echo strlen("Hello world<br>");
+        ?>
+    </body>
+</html>
